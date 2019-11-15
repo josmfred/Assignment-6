5d2500b4e9d817ca165ed397e397c1070da9fbd1
josmfred <josmfred@hotmail.com>
2019-11-15 15:28:55 -0800

    deleted top

--- src/interp.jl
+++ src/interp.jl
@@ -29,12 +29,14 @@ struct AppC{T}
 end
 
 ExprC = Union{NumC, StrC, IdC, CondC, LamC, AppC}
-
 Env = AbstractDict{String, Value}
+top
 
 function interp(expr :: ExprC, env :: Env) :: Real
     @match expr begin
         NumC(num) => NumV(num)
         Str(str) => StrV(str)
-        IdC(sym) =>
+        IdC(sym) => error("IdC not yet implemented")
+        LamC(params, body) => error("LamC not yet implemented")
+        AppC(fun, args) => error("AppC not yet implemented")
 end
