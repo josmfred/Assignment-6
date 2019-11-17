cb33019f4cb673064605bbe354c87863343fb93c
won young son <wonyoungson@wons-MacBook-Pro.local>
2019-11-16 20:18:23 -0800

    few helpers

--- src/parse.jl
+++ src/parse.jl
@@ -1,7 +1,25 @@
-function check_str(num)
+Pkg.add("Match")
+
+function checknumstr(sexp :: String) :: Bool
     try
-        parse(Float64,num)
-        true
-    catch
-        false
+        num = parse(Float64, sexp)
+        if isa(num, Real)
+            true
+        end
+    catch err
+        if isa(err, ArgumentError)
+            false
+        end
     end
+end
+
+println(checknumstr("123"))
+println(checknumstr("123a"))
+
+function parsenumstr(sexp :: String) :: Real
+    if (checknumstr(sexp))
+        parse(Float64, sexp)
+    end
+end
+
+println(parsenumstr("123"))
